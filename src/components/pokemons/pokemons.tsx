5fd90c6c4190ff5c94aff03550b42bb00b83da5a
Yura <yuraborisov19@gmail.com>
2021-10-25 16:59:33 +0300

    8. Paginator UI

--- src/components/pokemons/pokemons.tsx
+++ src/components/pokemons/pokemons.tsx
@@ -6,7 +6,7 @@ import style from './pokemons.module.css';
 const Pokemons: React.FC<any> = React.memo((props) => {
   console.log('POKEMONS', props)
   const [pokemonsArray, setPokemons] = React.useState<any>(null);
-  const [page, setPage] = React.useState(props.currentPage > 5 ? props.currentPage - 4 : props.currentPage);
+  const [page, setPage] = React.useState(props.currentPage > 5 ? props.currentPage - 6 : props.currentPage);
   const [value, setValue] = React.useState('');
 
   React.useEffect(() => {
@@ -57,36 +57,44 @@ const Pokemons: React.FC<any> = React.memo((props) => {
 
   return <div className={style.pokemons}>
     <div className={style.navigation}>
-      <div>
-        <button disabled={page <= 5} onClick={() => { props.withPrev(); setPage(page - 5) }}> prev </button>
+      <div className={style.paginator}>
+        <button className={style.btn_prev} disabled={page <= 5} onClick={() => { props.withPrev(); setPage(page - 5) }}>&#60;</button>
         {arrPages.map((p: number, index) => {
           if (p == 1) {
             return (
               <span key={p} className={props.currentPage == p ? style.nav_activ_span : style.nav__span} onClick={() => { props.withGetPokemonsNavigation(props.maxCards, index, p, props.maxCards); setPage(1) }}>{p}</span>)
           }
-          else if (p >= page && p < page + 5) {
+          else if (p > page && p <= page + 5) {
             return (
               <span key={p} className={props.currentPage == p ? style.nav_activ_span : style.nav__span} onClick={() => props.withGetPokemonsNavigation(props.maxCards, index, p, props.maxCards)}>{p}</span>)
           }
           else if (p == arrPages.length) {
-            return (
-              <span key={p}><span>...</span><span className={props.currentPage == p ? style.nav_activ_span : style.nav__span} onClick={() => { props.withGetPokemonsNavigation(props.maxCards, index, p, props.maxCards); setPage(arrPages.length - 5) }}>{p}</span></span>)
+            if (page == arrPages.length - 6) {
+              return (
+                <span key={p}><span className={props.currentPage == p ? style.nav_activ_span : style.nav__span} onClick={() => { props.withGetPokemonsNavigation(props.maxCards, index, p, props.maxCards); setPage(arrPages.length - 6) }}>{p}</span></span>)
+            }
+            else
+              return (
+                <span key={p}><span>...</span><span className={props.currentPage == p ? style.nav_activ_span : style.nav__span} onClick={() => { props.withGetPokemonsNavigation(props.maxCards, index, p, props.maxCards); setPage(arrPages.length - 6) }}>{p}</span></span>)
           }
         })}
 
-        <button disabled={page > arrPages.length - 6} onClick={() => { props.withNext(); setPage(page + 5) }}>next</button>
+        <button className={style.next_btn} disabled={page > arrPages.length - 7} onClick={() => { props.withNext(); setPage(page + 5) }}>&#62;</button>
+        <select className={style.select__btn} defaultValue='10' onChange={(event) => { props.withSetMaxCards(+event.target.value); setPage(1) }}>
+          <option value='10'>10</option>
+          <option value='20'>20</option>
+          <option value='50'>50</option>
+        </select>
       </div>
-      <select defaultValue='10' onChange={(event) => { props.withSetMaxCards(+event.target.value); setPage(1) }}>
-        <option value='10'>10</option>
-        <option value='20'>20</option>
-        <option value='50'>50</option>
-      </select>
+
+
       <div className={style.filter__block}>
         <input type="text" onChange={(event) => setValue(event.target.value)} />
         <button onClick={() => sortedBy('weight', 'dec')}>weigth</button>
         <button onClick={() => sortedBy('height', 'inc')}>height</button>
       </div>
     </div>
+
     <div className={style.card__wrapper}>
       {filterPokemons.map((pok: any) => { return <Pokemon key={pok.id} data={pok} /> })}
     </div>
